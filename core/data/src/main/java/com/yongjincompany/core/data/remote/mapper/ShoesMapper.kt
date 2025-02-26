2ce82e2048e1307ed52ce6b1b4adb11cee0db8ba
yongjin <dragon02229@naver.com>
2025-02-27 02:07:18 +0900

    ShoesRepositoryImpl에 캐싱 로직 구현 캐싱 로직으로 인한 Mapper 추가

--- core/data/src/main/java/com/yongjincompany/core/data/remote/mapper/ShoesMapper.kt
+++ core/data/src/main/java/com/yongjincompany/core/data/remote/mapper/ShoesMapper.kt
@@ -1,9 +1,28 @@
 package com.yongjincompany.core.data.remote.mapper
 
+import com.yongjincompany.core.data.local.entity.ShoesEntity
 import com.yongjincompany.core.data.remote.model.response.ShoesResponse
-import com.yongjincompany.core.domain.entity.ShoesEntity
+import com.yongjincompany.core.domain.entity.Shoes
 
-internal fun ShoesResponse.responseToEntity(): ShoesEntity =
+internal fun ShoesResponse.responseToEntity(): Shoes =
+    Shoes(
+        id,
+        name,
+        brandName,
+        imageUrl,
+        bookmarkCount
+    )
+
+internal fun ShoesEntity.localEntityToDomainEntity(): Shoes =
+    Shoes(
+        id,
+        name,
+        brandName,
+        imageUrl,
+        bookmarkCount
+    )
+
+internal fun ShoesResponse.responseToLocalEntity(): ShoesEntity =
     ShoesEntity(
         id,
         name,
