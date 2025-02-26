ad665b25d5a5f38b6ee1dfa1517d4253b4ffe5a0
yongjin <dragon02229@naver.com>
2025-02-27 02:05:39 +0900

    Local 로직에 대한 DI 설정

---
+++ core/data/src/main/java/com/yongjincompany/core/data/di/DataBaseModule.kt
@@ -0,0 +1,32 @@
+package com.yongjincompany.core.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.yongjincompany.core.data.local.SowDatabase
+import com.yongjincompany.core.data.local.dao.ShoesDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): SowDatabase {
+        return Room.databaseBuilder(
+            context,
+            SowDatabase::class.java,
+            "sow_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideShoesDao(database: SowDatabase): ShoesDao {
+        return database.shoesDao()
+    }
+}
