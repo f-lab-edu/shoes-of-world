77800560f26aaae2fdbe5dc9a5204d0d7aa75ab4
yongjin <dragon02229@naver.com>
2025-03-19 19:29:40 +0900

    hilt compile error해결
    data 모듈에 선언한 hilt 관련 모듈을 인식하지 못해 발생

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 dependencies {
     implementation(projects.feature.home)
     implementation(projects.core.designsystem)
+    implementation(projects.core.data)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
