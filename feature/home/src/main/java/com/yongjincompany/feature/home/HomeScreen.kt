46d95b7e5e4f158a2dbae36313a2fb240ec7197a
yongjin <dragon02229@naver.com>
2025-03-19 22:05:10 +0900

    List sample 구현

--- feature/home/src/main/java/com/yongjincompany/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/yongjincompany/feature/home/HomeScreen.kt
@@ -1,10 +1,15 @@
 package com.yongjincompany.feature.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -19,6 +24,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
 import com.yongjincompany.core.designsystem.theme.SowTheme
 import com.yongjincompany.core.domain.entity.Banner
+import com.yongjincompany.core.domain.entity.Shoes
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltViewModel()) {
@@ -30,7 +36,8 @@ fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltVie
         viewModel.fetchBannerList()
     }
 
-    Box(modifier) {
+    Column(modifier) {
+
         //TODO: top app bar
 
         //TODO: banner
@@ -38,6 +45,7 @@ fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltVie
         //TODO: category
 
         //TODO: shoes list
+        HomeShoesList(shoesList = shoesList)
     }
 }
 
@@ -63,24 +71,83 @@ fun HomeBanner(modifier: Modifier = Modifier, bannerList: List<Banner>) {
     }
 }
 
+@Composable
+fun HomeShoesList(modifier: Modifier = Modifier, shoesList: List<Shoes>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+    ) {
+        items(shoesList, key = { it.id }) { shoe ->
+            ShoeItem(shoe)
+        }
+    }
+}
+
+@Composable
+fun ShoeItem(shoe: Shoes) {
+    Text(text = shoe.name)
+}
+
 
 @Preview
 @Composable
 fun PreviewHomeBanner() {
     SowTheme {
         val mockBannerList = listOf(
-            Banner(0, "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw"),
-            Banner(1, "https://fastly.picsum.photos/id/859/200/300.jpg?hmac=Ty9c6r04FYgwfkw5JMqSLG_eMq0RxeaMTAArotTX6Qk"),
-            Banner(2, "https://fastly.picsum.photos/id/179/200/300.jpg?hmac=oo9H3-mvUxV9CjfSms5helxQW-n5PsZLzkg1ko78uFk"),
+            Banner(
+                0,
+                "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw"
+            ),
+            Banner(
+                1,
+                "https://fastly.picsum.photos/id/859/200/300.jpg?hmac=Ty9c6r04FYgwfkw5JMqSLG_eMq0RxeaMTAArotTX6Qk"
+            ),
+            Banner(
+                2,
+                "https://fastly.picsum.photos/id/179/200/300.jpg?hmac=oo9H3-mvUxV9CjfSms5helxQW-n5PsZLzkg1ko78uFk"
+            ),
         )
         HomeBanner(bannerList = mockBannerList)
     }
 }
 
+
+@Preview
+@Composable
+fun PreviewHomeShoesList() {
+    SowTheme {
+        val mockShoesList = listOf(
+            Shoes(
+                id = 0,
+                imageUrl = "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw",
+                brandName = "",
+                name = "",
+                bookmarkCount = 1
+            ),
+            Shoes(
+                id = 1,
+                imageUrl = "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw",
+                brandName = "",
+                name = "",
+                bookmarkCount = 1
+            ),
+            Shoes(
+                id = 2,
+                imageUrl = "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw",
+                brandName = "",
+                name = "",
+                bookmarkCount = 1
+            ),
+        )
+        HomeShoesList(shoesList = mockShoesList)
+    }
+}
+
+
 @Preview
 @Composable
 fun PreviewHomeScreen() {
     SowTheme {
-        HomeScreen()
+        //HomeScreen()
     }
 }
