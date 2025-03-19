aee3005bfc5588e42ca07a3c1422aa422eb628e5
yongjin <dragon02229@naver.com>
2025-03-19 21:25:13 +0900

    HomeViewModel에 배너와 신발리스트 불러오는 메서드 구현

--- feature/home/src/main/java/com/yongjincompany/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/yongjincompany/feature/home/HomeViewModel.kt
@@ -2,34 +2,48 @@ package com.yongjincompany.feature.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.yongjincompany.core.domain.usecase.FetchAllShoesUseCase
+import com.yongjincompany.core.domain.entity.Banner
+import com.yongjincompany.core.domain.entity.Shoes
+import com.yongjincompany.core.domain.usecase.banner.FetchBannerListUseCase
+import com.yongjincompany.core.domain.usecase.shoes.FetchAllShoesListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-/*@HiltViewModel
+@HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val fetchAllShoesUseCase: FetchAllShoesUseCase
+    private val fetchAllShoesUseCase: FetchAllShoesListUseCase,
+    private val fetchBannerListUseCase: FetchBannerListUseCase
 ) : ViewModel() {
-    private val _event = MutableSharedFlow<HomeEvent>()
-    val event = _event.asSharedFlow()
+
+    private val _shoesListState = MutableStateFlow<List<Shoes>>(emptyList())
+    val shoesListState = _shoesListState.asStateFlow()
+
+    private val _bannerListState = MutableStateFlow<List<Banner>>(emptyList())
+    val bannerListState = _bannerListState.asStateFlow()
 
     fun fetchAllShoesList() {
-        viewModelScope.launch() {
-            runCatching {
-                fetchAllShoesUseCase()
-            }.onSuccess {
-                _event.emit(FetchAllShoesListSuccess(it))
-            }.onFailure {
-                _event.emit(HomeFailed(R.string.cant_load_shoes_list_plz_retry))
+        viewModelScope.launch {
+            fetchAllShoesUseCase()
+                .flowOn(Dispatchers.IO)
+                .collect { shoesList ->
+                _shoesListState.value = shoesList
+            }
+        }
+    }
+
+    fun fetchBannerList() {
+        viewModelScope.launch {
+            fetchBannerListUseCase()
+                .flowOn(Dispatchers.IO)
+                .collect { bannerList ->
+                _bannerListState.value = bannerList
             }
         }
     }
 }
-
-sealed interface HomeEvent
-
-data class FetchAllShoesListSuccess(val data: List<Shoes>) : HomeEvent
-data class HomeFailed(val errorMessage: Int) : HomeEvent*/
