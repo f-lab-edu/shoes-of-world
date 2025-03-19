b04a47114073891eacd3f210a4f87ba1f008e853
yongjin <dragon02229@naver.com>
2025-03-19 21:26:49 +0900

    HomeBanner 구현

--- feature/home/src/main/java/com/yongjincompany/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/yongjincompany/feature/home/HomeScreen.kt
@@ -1,10 +1,94 @@
 package com.yongjincompany.feature.home
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import com.yongjincompany.core.designsystem.theme.SowTheme
+import com.yongjincompany.core.domain.entity.Banner
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Text("Shoes-Of-World")
+fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltViewModel()) {
+    val shoesList by viewModel.shoesListState.collectAsState()
+    val bannerList by viewModel.bannerListState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchAllShoesList()
+        viewModel.fetchBannerList()
+    }
+
+    Box(modifier) {
+        //TODO: top app bar
+
+        //TODO: banner
+        HomeBanner(bannerList = bannerList)
+        //TODO: category
+
+        //TODO: shoes list
+    }
+}
+
+@Composable
+fun HomeBanner(modifier: Modifier = Modifier, bannerList: List<Banner>) {
+    val context = LocalContext.current
+    val pagerState = rememberPagerState(pageCount = { bannerList.size })
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = modifier.height(390.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = bannerList[it].imageUrl,
+            contentDescription = context.getString(R.string.content_description_banner),
+            contentScale = ContentScale.Crop,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewHomeBanner() {
+    SowTheme {
+        val mockBannerList = listOf(
+            Banner(0, "https://fastly.picsum.photos/id/496/200/300.jpg?hmac=demLRv0UMwDhQHH6AEmbkJqlYuX27lnRH5N9FYcHBgw"),
+            Banner(1, "https://fastly.picsum.photos/id/859/200/300.jpg?hmac=Ty9c6r04FYgwfkw5JMqSLG_eMq0RxeaMTAArotTX6Qk"),
+            Banner(2, "https://fastly.picsum.photos/id/179/200/300.jpg?hmac=oo9H3-mvUxV9CjfSms5helxQW-n5PsZLzkg1ko78uFk"),
+        )
+        HomeBanner(bannerList = mockBannerList)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    SowTheme {
+        HomeScreen()
+    }
 }
